c3477042f343504499bd0919dfea8cb19403f14c
Sean Hickey <tallirishllama@yahoo.com>
2012-08-24 23:56:33 -0700

    We've got movement

--- src/Character.java
+++ src/Character.java
@@ -1,17 +1,29 @@
 public class Character {
+    public enum Directions {NORTH,SOUTH,EAST,WEST,NORTHWEST,NORTHEAST,SOUTHWEST,SOUTHEAST};
+
     protected float xPosition, yPosition;
-    protected boolean alive;
+    protected boolean alive, isDrawn;
+    protected float[] color;//temporary until we have graphics, just color of square
+    protected float xVelocity, yVelocity;
     
-    public Character(float x, float y) {
+    private static float[] defaultColor = {1,1,1};
+    
+    public Character(float x, float y, float[] c) {
 	xPosition = x;
 	yPosition = y;
 	alive = true;
+	isDrawn = false;
+	color = c;
+	xVelocity = 5;
+	yVelocity = 5;
+    }
+
+    public Character(float x, float y) {
+	this(x,y, defaultColor);
     }
 
     public Character() {
-	xPosition = 100;
-	yPosition = 100;
-	alive = true;
+	this(100,100);
     }
 
     public boolean isAlive() {
@@ -25,6 +37,55 @@ public class Character {
     public void move(float x, float y) {
 	xPosition += x;
 	yPosition += y;
+    }
+    
+    public void move(Directions d) {
+	switch(d) {
+	case NORTH:
+	    yPosition += yVelocity;
+	    break;
+	case WEST:
+	    xPosition -= xVelocity;
+	    break;
+	case EAST:
+	    xPosition += xVelocity;
+	    break;
+	case SOUTH:
+	    yPosition -= yVelocity;
+	    break;
+	case NORTHEAST:
+	    xPosition += xVelocity;
+	    yPosition += yVelocity;
+	    break;
+	case NORTHWEST:
+	    xPosition -= xVelocity;
+	    yPosition += yVelocity;
+	    break;
+	case SOUTHEAST:
+	    xPosition += xVelocity;
+	    yPosition -= yVelocity;
+	    break;
+	case SOUTHWEST:
+	    xPosition -= xVelocity;
+	    yPosition -= yVelocity;
+	    break;
+	}
+    }
+
+    public float getXPos() {
+	return xPosition;
+    }
+    public float getYPos() {
+	return yPosition;
+    }
+    public float getXVel() {
+	return xVelocity;
+    }
+    public float getYVel() {
+	return yVelocity;
+    }
+    public float[] getColor() {
+	return color;
     }
 
 }
