9a44c2a932561504073da0e7257cc3e7d6ad1014
Ronald Lee <rwlee5168@gmail.com>
2012-08-24 23:15:50 -0700

    Adding map code

---
+++ src/Cell.java
@@ -0,0 +1,15 @@
+public class Cell(){
+    public final int pixel_length = 6;
+    public final int pixel_width = 6;
+    Object container = null;
+    public Cell(){
+    }
+
+    public void place(Object object){
+	container = object;
+    }
+    public Object get(){
+	return container;
+    }
+}
+	
